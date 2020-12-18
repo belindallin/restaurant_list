df87b3ecad5d38328e7de8cd8adf4831933c470f
belindalin <dog83919814@yahoo.com.tw>
2020-12-18 22:02:29 +0800

    modify show data

--- app.js
+++ app.js
@@ -52,9 +52,14 @@ app.post ( '/restaurant' , ( req ,res ) => {
         .catch(error => console.log(error))
 
 })
-app.get ( '/restaurants/:restaurant_id' , ( req , res ) => {
-    const restaurant= restaurant_list.results.find ( restaurant => restaurant.id.toString() === req.params.restaurant_id)
-    res.render ( 'show', { restaurant: restaurant } )
+app.get ( '/restaurants/:id' , ( req , res ) => {
+    const id = req.params.id
+    return Restaurant.findById (id)
+    .lean()
+    .then( restaurant  => {
+        res.render( 'show' , { restaurant })
+    })
+    .catch(error => console.log(error))
 })
 app.get ( '/search' , ( req , res ) => {
     const keyword = req.query.keyword
