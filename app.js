151924fc05b77183c54187d9cc89d7cfeae92182
belindalin <dog83919814@yahoo.com.tw>
2020-12-18 23:55:04 +0800

    add remove and update restaurant

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const Restaurant = require ( './models/restaurant' )
 const mongoose = require ( 'mongoose' )
 //load body-parser
 const bodyParser = require ( 'body-parser' )
+const restaurantList = require('./restaurant.json').results
 
 //Declare related variables for server
 const app = express ()
@@ -61,13 +62,57 @@ app.get ( '/restaurants/:id' , ( req , res ) => {
     })
     .catch(error => console.log(error))
 })
+app.get ( '/restaurants/:id/edit' , ( req , res ) => {
+    const id = req.params.id
+    return Restaurant.findById (id)
+    .lean()
+    .then( restaurant  =>  res.render( 'edit' , { restaurant }))
+    .catch(error => console.log ( error ))
+})
+
+app.post ( '/restaurants/:id/edit' , ( req , res ) => {
+    const id = req.params.id
+    const name = req.body.name     
+    const name_en = req.body.name_en  
+    const category  = req.body.category   
+    const image  = req.body.image   
+    const location = req.body.location  
+    const phone = req.body.phone  
+    const google_map = req.body.google_map  
+    const rating  = req.body.rating  
+    const description = req.body.description 
+    
+    return Restaurant.findById('id')
+        .then ( restaurant => {
+            restaurant.name = name
+            restaurant.name_en = name_en 
+            restaurant.category = category
+            restaurant.image = image 
+            restaurant.location = location 
+            restaurant.phone = phone 
+            restaurant.google_map = google_map 
+            restaurant.rating = rating 
+            restaurant.description = description
+            return Restaurant.save()
+        })
+        .then(() => res.redirect('/restaurants/${id}')) 
+        .catch(error => console.log(error))
+})
+app.post('/restaurants/:id/delete', (req, res) => {
+    const id = req.params.id
+    return Restaurant.findById ( id )
+      .then ( restaurant => restaurant.remove())
+      .then ( () => res.redirect('/'))
+      .catch ( error => console.log(error))
+})
 app.get ( '/search' , ( req , res ) => {
     const keyword = req.query.keyword
     const restaurants = restaurant_list.results.filter ( restaurant => restaurant.name.toLowerCase().includes(keyword.toLowerCase().trim()) || restaurant.category.toLowerCase().includes(keyword.toLowerCase().trim()) )   
     res.render ( 'index', { restaurants: restaurants , keywords :  req.query.keyword } )
 })
 
+
 //listening server
 app.listen ( port , () => {
-    console.log ( `Resaurant Web is running on http://localhost:${port}` )
+    console.log ( `Restaurant Web is running on http://localhost:${port}` )
 })
