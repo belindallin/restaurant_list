e180d0d7c1a60bec9e00e7f874c7fb9e26d50d94
belindalin <dog83919814@yahoo.com.tw>
2020-12-18 20:05:03 +0800

    add restaurant seeder

--- models/restaurant.js
+++ models/restaurant.js
@@ -7,39 +7,27 @@ const restaurantSchema = new Schema ({
     } ,
     name_en : {
         type : String ,
-        required : true ,
     } ,
     category : {
         type : String ,
-        required : true ,
     } ,
     image : {
         type : String ,
-        required : true ,
     } ,
     location : {
         type : String ,
-        required : true ,
     } ,
     phone : {
         type : String ,
-        required : true ,
-    } ,
-    phone : {
-        type : String ,
-        required : true ,
     } ,
     google_map : {
         type : String ,
-        required : true ,
     } ,
     rating : {
         type : String ,
-        required : true ,
     } ,
     description : {
         type : String ,
-        required : true ,
     }   
 })
 
