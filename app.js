06a73f7e05b79a6848332f033ca64f85141880d8
belinda <dog83919814@yahoo.com.tw>
2020-12-24 15:41:57 +0800

    feat :  app.js by JavaScript Standard Style

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const Restaurant = require ( './models/restaurant' )
 const mongoose = require ( 'mongoose' )
 //load body-parser
 const bodyParser = require ( 'body-parser' )
-const restaurantList = require('./restaurant.json').results
+const restaurantList = require ( './restaurant.json' ).results
 
 //Declare related variables for server
 const app = express ()
@@ -17,22 +17,22 @@ const port = 3000
 app.engine ( 'handlebars' , exphbs ({ defaultLayout : 'main' }) )
 app.set ( 'view engine' , 'handlebars' )
 app.use ( express.static ( 'public' ) )
-app.use ( bodyParser.urlencoded ({ extended : true }))
+app.use ( bodyParser.urlencoded ({ extended : true }) )
 
 //set connection with database
 mongoose.connect ( 'mongodb://localhost/restaurant_list' , { useNewUrlParser: true , useUnifiedTopology: true } )
 
 const db = mongoose.connection
-db.on ( 'error' , () => { console.log ('mongodb error!!!')})
+db.on ( 'error' , () => { console.log ('mongodb error!!!') } )
 db.once ( 'open' , () => { 
-    console.log ('mongoose connected')
+    console.log ( 'mongoose connected' )
 })
 
 //get data by client and response to client
 app.get ( '/' , ( req , res ) => {
     Restaurant.find()
     .lean()
-    .then ( restaurants => res.render ( 'index', { restaurants }) )
+    .then ( restaurants => res.render ( 'index', { restaurants } ) )
     .catch ( error => console.log ( error ) )    
 })
 app.get ( '/restaurant/new' , ( req , res ) => {
@@ -48,30 +48,29 @@ app.post ( '/restaurant' , ( req ,res ) => {
     const google_map = req.body.google_map  
     const rating  = req.body.rating  
     const description = req.body.description    
-    return Restaurant.create({ name , name_en , category , image , location , phone ,google_map , rating ,description })     
-        .then(() => res.redirect('/')) 
-        .catch(error => console.log(error))
+    return Restaurant.create ({ name , name_en , category , image , location , phone ,google_map , rating ,description })     
+        .then ( () => res.redirect ('/') ) 
+        .catch ( error => console.log (error) )
 
 })
 app.get ( '/restaurants/:id' , ( req , res ) => {
     const id = req.params.id
-    return Restaurant.findById (id)
+    return Restaurant.findById ( id )
     .lean()
-    .then( restaurant  => {
-        res.render( 'show' , { restaurant })
+    .then ( restaurant  => {
+        res.render( 'show' , { restaurant } )
     })
     .catch(error => console.log(error))
 })
 app.get ( '/restaurants/:id/edit' , ( req , res ) => {
     const id = req.params.id
-    return Restaurant.findById (id)
+    return Restaurant.findById ( id )
     .lean()
-    .then( restaurant  =>  res.render( 'edit' , { restaurant }))
-    .catch(error => console.log ( error ))
+    .then ( restaurant  =>  res.render ( 'edit' , { restaurant } ) )
+    .catch ( error => console.log ( error ) )
 })
 
 app.post ( '/restaurants/:id/edit' , ( req , res ) => {
-    console.log ( req.body )
     const id = req.params.id
     const name = req.body.name     
     const name_en = req.body.name_en  
@@ -96,19 +95,19 @@ app.post ( '/restaurants/:id/edit' , ( req , res ) => {
             restaurant.description = description
             return restaurant.save()
         })
-        .then(() => res.redirect(`/restaurants/${id}`)) 
-        .catch(error => console.log(error))
+        .then ( () => res.redirect ( `/restaurants/${id}` ) ) 
+        .catch ( error => console.log ( error ) )
 })
-app.post('/restaurants/:id/delete', (req, res) => {
+app.post ( '/restaurants/:id/delete' , ( req , res ) => {
     const id = req.params.id
     return Restaurant.findById ( id )
-      .then ( restaurant => restaurant.remove())
-      .then ( () => res.redirect('/'))
-      .catch ( error => console.log(error))
+      .then ( restaurant => restaurant.remove () )
+      .then ( () => res.redirect ('/') )
+      .catch ( error => console.log ( error ) )
 })
 app.get ( '/search' , ( req , res ) => {
     const keyword = req.query.keyword
-    const restaurants = restaurant_list.results.filter ( restaurant => restaurant.name.toLowerCase().includes(keyword.toLowerCase().trim()) || restaurant.category.toLowerCase().includes(keyword.toLowerCase().trim()) )   
+    const restaurants = restaurantList.filter ( restaurant => restaurant.name.toLowerCase().includes ( keyword.toLowerCase().trim() ) || restaurant.category.toLowerCase().includes ( keyword.toLowerCase().trim() ) )   
     res.render ( 'index', { restaurants: restaurants , keywords :  req.query.keyword } )
 })
 
